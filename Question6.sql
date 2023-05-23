e3f5cb62dcac901ab2bac5e79555c8f51970e448
jyoti8399 <127869450+jyoti8399@users.noreply.github.com>
2023-05-23 12:52:11 +0530

    Question6.sql

--- Question6.sql
+++ Question6.sql
@@ -1,6 +1,25 @@
-select sp_p2.cat_id, sp_p2.cat_name, sp_p2.pro_name as 'product name', sp_p2.supp_price as 'product price' from (select c.*, sp_p.pro_id, sp_p.pro_name, sp_p.supp_price, rank() over(partition by c.cat_id order by sp_p.supp_price) as price_rank from category c inner join
-(select p.*, sp.supp_price from supplier_pricing sp inner join
-product p
-on sp.pro_id = p.pro_id) as SP_P
-on c.cat_id = sp_p.cat_id) as SP_P2
-where price_rank = 1
+SELECT
+  sp_p2.cat_id,
+  sp_p2.cat_name,
+  sp_p2.pro_name AS 'product name',
+  sp_p2.supp_price AS 'product price'
+FROM (
+  SELECT
+    c.*,
+    sp_p.pro_id,
+    sp_p.pro_name,
+    sp_p.supp_price,
+    RANK() OVER (
+      PARTITION BY c.cat_id
+      ORDER BY sp_p.supp_price
+    ) AS price_rank
+  FROM category c
+  INNER JOIN (
+    SELECT p.*, sp.supp_price
+    FROM supplier_pricing sp
+    INNER JOIN product p
+    ON sp.pro_id = p.pro_id
+  ) AS SP_P
+  ON c.cat_id = sp_p.cat_id
+) AS SP_P2
+WHERE price_rank = 1;
